9231c3ff78f3f4e964c0d09b53c874e5de7bacba
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-19 02:00:57 -0300

    removing route

--- app.py
+++ app.py
@@ -107,10 +107,5 @@ def delete_user(id_user):
     return jsonify({"message": "Usuário não encontrado"})
 
 
-@app.route("/ola", methods=['GET'])
-def hello_world():
-    return "Hello World!"
-
-
 if __name__ == '__main__':
     app.run(debug=True)
