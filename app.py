327fe1656a0393b2f7782f2a11aafe48279ac6e8
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-18 23:02:15 -0300

    feat: create user view

--- app.py
+++ app.py
@@ -46,7 +46,22 @@ def login():
 def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesos!"})
-     
+
+
+@app.route("/user", methods=['POST'])
+@login_required
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário criado com sucesso!"})
+    
+    return jsonify({"message": "Dados inválidos"}), 401
 
 @app.route("/ola", methods=['GET'])
 def hello_world():
